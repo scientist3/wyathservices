b01b39d38ff85d579cb06acfe37e088a05abb1bb
Er Anees Akbar <99726488+eranees@users.noreply.github.com>
2022-02-27 11:36:53 +0530

    test anees

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,7 +1,8 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Welcome extends CI_Controller
+{
 
 	/**
 	 * Index Page for this controller.
@@ -20,6 +21,7 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
+		echo "Hello world";
 		$this->load->view('welcome_message');
 	}
 }
