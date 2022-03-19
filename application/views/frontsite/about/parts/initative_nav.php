962f1e92ec745fb7c96933bb2fa301cd4fefe2dc
Aamir Sofi <eraamirsofi@gmail.com>
2022-03-19 10:30:08 +0530

    Fixed menu and added initatiives + services and contact page

---
+++ application/views/frontsite/about/parts/initative_nav.php
@@ -0,0 +1,72 @@
+<!-- BEGIN: LAYOUT/BREADCRUMBS/BREADCRUMBS-3 -->
+<div class="c-layout-breadcrumbs-1 c-bgimage c-subtitle c-fonts-uppercase c-fonts-bold c-bg-img-center" style="background-image: url(<?= base_url('frontsite/'); ?>assets/base/img/content/backgrounds/bg1.jpg)">
+  <div class="container">
+    <div class="c-page-title c-pull-left">
+      <h3 class="c-font-uppercase c-font-bold c-font-white c-font-20 c-font-slim">Initatives</h3>
+      <h4 class="c-font-white c-font-thin c-opacity-07">
+      </h4>
+    </div>
+    <ul class="c-page-breadcrumbs c-theme-nav c-pull-right c-fonts-regular">
+      <li>
+        <a href="<?= base_url(''); ?>" class="c-font-white">Home</a>
+      </li>
+      <li class="c-font-white">
+        /
+      </li>
+      <li>
+        <a href="<?= base_url('front/initiatives'); ?>" class="c-font-white">Initatives</a>
+      </li>
+      <li class="c-font-white">
+        /
+      </li>
+      <li class="c-state_active c-font-brown-3">
+        Initatives
+      </li>
+    </ul>
+  </div>
+</div>
+
+<div id="sticky-anchor"></div>
+<div id="sticky">
+  <div class="c-layout-breadcrumbs-1 c-font-bold c-bordered c-bordered-both">
+    <div class="container" style="text-align:center;">
+
+      <ul class="c-page-breadcrumbs c-theme-nav c-font-open  c-font-13 c-font-thin">
+        <li>
+          <a href="Initiatives.html" class="c-font-brown-3">Initiatives</a>
+        </li>
+        <li>
+          |
+        </li>
+
+        <li class="c-state_active">
+          <a href="#">Training & Development</a>
+        </li>
+        <li>
+          |
+        </li>
+
+        <li class="c-state_active">
+          <a href="#">Consulting</a>
+        </li>
+        <li>
+          |
+        </li>
+        <li class="c-state_active">
+          <a href="#">Wyath Life</a>
+        </li>
+        <li>
+          |
+        </li>
+        <li class="c-state_active">
+          <a href="#">Infrastructure Development</a>
+        </li>
+
+
+      </ul>
+
+
+    </div>
+  </div>
+</div>
+<!-- END: LAYOUT/BREADCRUMBS/BREADCRUMBS-3 -->
---
+++ application/views/frontsite/about/services.php
@@ -0,0 +1,218 @@
+<!-- BEGIN: PAGE CONTAINER -->
+<div class="c-layout-page">
+  <!-- BEGIN: LAYOUT/BREADCRUMBS/BREADCRUMBS-3 -->
+  <div class="c-layout-breadcrumbs-1 c-bgimage c-subtitle c-fonts-uppercase c-fonts-bold c-bg-img-center" style="background-image: url(<?= base_url('frontsite/'); ?>assets/base/img/content/backgrounds/bg1.jpg)">
+    <div class="container">
+      <div class="c-page-title c-pull-left">
+        <h3 class="c-font-uppercase c-font-bold c-font-white c-font-20 c-font-slim">Services</h3>
+        <h4 class="c-font-white c-font-thin c-opacity-07">
+        </h4>
+      </div>
+      <ul class="c-page-breadcrumbs c-theme-nav c-pull-right c-fonts-regular">
+        <li>
+          <a href="index-2.html" class="c-font-white">Home</a>
+        </li>
+        <li class="c-font-white">
+          /
+        </li>
+
+        <li class="c-state_active c-font-brown-3">Services
+        </li>
+
+
+      </ul>
+    </div>
+  </div>
+
+
+  <!-- END: LAYOUT/BREADCRUMBS/BREADCRUMBS-3 -->
+  <!-- BEGIN: PAGE CONTENT -->
+
+  <div class="c-content-box c-size-sm1 c-bg-white">
+    <div class="container">
+      <div class="c-content-product-1 c-opt-1">
+        <div class="c-content-title-1">
+          <h3 class="c-center c-font-uppercase c-font-bold">WORKING IN DIFFICULT GEOGRAPHIES</h3>
+          <p class="c-font-uppercase c-center">
+            OUR STRONG COMMITMENT TOWARDS TAKING SKILLS AND
+            LIVELIHOOD OPPORTUNITIES TO EVERY CORNER OF INDIA
+            HAS HELPED US SET UP TOUCH POINTS IN SOME OF THE MOST
+            DIFFICULT GEOGRAPHIES, REMOTE AREAS AND BACKWARD
+            REGIONS.
+            WE ARE ONE OF THE FIRST ORGANIZATIONS TO LAUNCH SKILLS
+            PROGRAMMES IN THESE GEOGRAPHIES.
+            CURRENTLY WE ARE OPERATING IN 14 DISTRICTS OF THE UT OF
+            JAMMU AND KASHMIR.
+          </p>
+          <div class="c-line-center">
+          </div>
+
+
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+
+
+  <!-- BEGIN: CONTENT/FEATURES/FEATURES-4-3 -->
+  <!-- BEGIN: FEATURES 4.3 -->
+  <div id="1f" class="c-content-box c-size-md c-no-padding c-bg-white">
+    <div class="c-content-feature-4">
+      <div class="c-bg-parallax c-feature-bg c-content-right c-diagonal c-border-left-white" style="background-image: url(<?= base_url('frontsite/'); ?>assets/base/img/content/backgrounds/1.jpg)">
+      </div>
+      <div class="c-content-area c-content-left">
+      </div>
+      <div class="container">
+        <div class="c-feature-content c-left">
+          <div class="c-content-v-center">
+            <div class="c-wrapper">
+              <div class="c-body">
+                <div class="c-content-title-1">
+                  <h3 class="c-font-uppercase c-font-bold c-left">GOVERNMENT MANDATES</h3>
+                  <div class="c-line-left">
+                  </div>
+
+                </div>
+                <div class="c-content-title-1">
+                  <p class="c-font-open c-justify">
+                    This Skills Schools focuses on the implementation of skill development
+                    programmes funded by various ministries/departments under central &
+                    state Governments across the country.<br>
+                    Some of the key schemes/ ministries/ departments/ State Governments, we
+                    have worked with are :<br>
+                    <br> • Pradhan Mantri Kaushal Vikas Yojana (PMKVY 2.0) under the Ministry of
+                    Skill Development & Entrepreneurship, Government of India & National
+                    Skills Development Corporation (NSDC)
+                    <br> • Pradhan Mantri Kaushal Vikas Yojana (PMKVY 2.0) - under the Jammu &
+                    Kashmir Skill Development Mission ( JKSDM) & National Skills
+                    Development Corporation (NSDC)
+                    <br> • Pradhan Mantri Kaushal Vikas Yojana (PMKVY 2.0) - under the Skill
+                    Council for Persons with Disabilities (SC-PwD).
+                    <br> • Deendayal Antyodaya Yojana - National Urban Livelihoods Mission
+                    EST&P(DAY-NULM) Under the Jammu & Kashmir Housing & Urban
+                    Development Mission.
+                    <br> • NSDC-CSR - Imparting Skill Development training to Indian youths for
+                    REC Foundation under National Skills Development Corporation (NSDC)
+                    <br> • NABARD Skill Initiative - Imparting Skill Development training to Indian
+                    youths under National Bank For Agriculture And Rural Development
+                    (NABARD)
+                    <br> • NSDC-RPL - Sports Skills Council Sponsored (SPEFL-SC) RPL in Sports
+                    Sector for the Physical Activity Instructors of Government Schools.
+                    <br> • Paid RPL in Food Processing Sector, Paid RPL in Handicrafts &
+                    Handloom Sector under the sponsorship of District Administration
+                    Kupwara Jammu & Kashmir.
+                    <br> • Free Online Crash Course in Hospitality Sector under the aegis of
+                    Jammu & Kashmir Skill Development Mission (JKSDM)
+                    <br> • Face Mask Making During COVID 19 for District Administration Kupwara
+                    & under the aegis of Jammu & Kashmir Skill Development Mission
+                    (JKSDM)
+
+
+
+                  </p>
+                  <a href="faculty-development.html" class="btn btn-sm c-theme-btn c-btn-square c-btn-sbold">
+                    EXPLORE </a>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- END: FEATURES 4.3 -->
+  <!-- END: CONTENT/FEATURES/FEATURES-4-3 -->
+  <!-- BEGIN: CONTENT/FEATURES/FEATURES-4-4 -->
+  <!-- BEGIN: FEATURES 4.4 -->
+  <div id="1s" class="c-content-box c-size-md c-no-padding c-bg-dark-2">
+    <div class="c-content-feature-4">
+      <div class="c-bg-parallax c-feature-bg c-content-left c-diagonal c-border-right-dark-2" style="background-image: url(<?= base_url('frontsite/'); ?>assets/base/img/content/backgrounds/2.jpg)">
+      </div>
+      <div class="c-content-area c-content-right">
+      </div>
+      <div class="container">
+        <div class="c-feature-content c-right">
+          <div class="c-content-v-center">
+            <div class="c-wrapper">
+              <div class="c-body">
+                <div class="c-content-title-1">
+                  <h3 class="c-font-uppercase c-font-bold c-font-white">RETAIL BUSINESS</h3>
+                  <div class="c-line-right">
+                  </div>
+
+                </div>
+                <div class="c-content-title-1">
+                  <p class="c-font-open c-justify">
+                    <strong> This unit focuses on implementation of skill development programmes funded by employer
+                      and/or paid by student for various programmes
+                    </strong><br />
+                    <br />(i) Student paid model - The fee is paid by the student himself
+
+                    <br />(ii) Employer funded - There are three partnership models -
+
+                    <br />• Hire and train - The candidate is hired (by way of Letter of Intent) before he/she
+                    enrols for the training programme. These are programmes exclusive to an employer
+                    <br />• Providing employment opportunities to our students
+                    <br />• Providing on–the–job training opportunities
+
+                  </p>
+                  <a href="skill-development-courses.html" class="btn btn-sm c-theme-btn c-btn-square c-btn-sbold">
+                    EXPLORE </a>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- END: FEATURES 4.4 -->
+  <!-- END: CONTENT/FEATURES/FEATURES-4-4 -->
+  <!-- BEGIN: CONTENT/FEATURES/FEATURES-4-3 -->
+  <!-- BEGIN: FEATURES 4.3 -->
+  <div id="1e" class="c-content-box c-size-md c-no-padding c-bg-white">
+    <div class="c-content-feature-4">
+      <div class="c-bg-parallax c-feature-bg c-content-right c-diagonal c-border-left-white" style="background-image: url(<?= base_url('frontsite/'); ?>assets/base/img/content/backgrounds/3.jpg)">
+      </div>
+      <div class="c-content-area c-content-left">
+      </div>
+      <div class="container">
+        <div class="c-feature-content c-left">
+          <div class="c-content-v-center">
+            <div class="c-wrapper">
+              <div class="c-body">
+                <div class="c-content-title-1">
+                  <h3 class="c-font-uppercase c-font-bold c-left">CORPORATE SOCIAL RESPONSIBILITY (CSR) MANDATES
+                  </h3>
+                  <div class="c-line-left">
+                  </div>
+
+                </div>
+                <div class="c-content-title-1">
+                  <p class="c-font-open c-justify">
+                    This Unit focuses on implementation of skill development programmes funded by Corporates as part
+                    of their CSR obligation. <br /><br />
+                    WSPL has an experience of successfully implementing CSR programmes for an array of learner
+                    segments in some of the most backward regions, difficult geographies, conflict zones, industrial
+                    clusters as well as urban areas across the country.
+                  </p>
+                  <a href="Entrepreneurship-development.html" class="btn btn-sm c-theme-btn c-btn-square c-btn-sbold">
+                    EXPLORE </a>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- END: FEATURES 4.3 -->
+
+  <!-- END: PAGE CONTENT -->
+</div>
+<!-- END: PAGE CONTAINER -->
