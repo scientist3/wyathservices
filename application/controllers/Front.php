355663ed8872197504df5d352c24882c2c2a880b
Aamir Sofi <eraamirsofi@gmail.com>
2022-03-20 12:17:46 +0530

    Fixed issue in initatives

--- application/controllers/Front.php
+++ application/controllers/Front.php
@@ -3,119 +3,119 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Front extends CI_Controller
 {
-  public function __construct()
-  {
-    parent::__construct();
+	public function __construct()
+	{
+		parent::__construct();
 
-    $this->load->model(array(
-      'FrontModel' => 'front_model',
-    ));
-  }
-  public function index()
-  {
-    $data['title']                = "Home";
-    $data['featured_initatives']  = $this->front_model->get_featured_initatives();
-    $data['projects']             = $this->front_model->get_projects();
-    $data['partners']             = $this->front_model->get_partners();
-    $data['sliders']              = $this->front_model->get_sliders();
-    $data['what_we_dos']          = $this->front_model->get_what_we_do();
-    $data['students_impacted']    = $this->front_model->get_students_impacted();
+		$this->load->model(array(
+			'FrontModel' => 'front_model',
+		));
+	}
+	public function index()
+	{
+		$data['title']                = "Home";
+		$data['featured_initatives']  = $this->front_model->get_featured_initatives();
+		$data['projects']             = $this->front_model->get_projects();
+		$data['partners']             = $this->front_model->get_partners();
+		$data['sliders']              = $this->front_model->get_sliders();
+		$data['what_we_dos']          = $this->front_model->get_what_we_do();
+		$data['students_impacted']    = $this->front_model->get_students_impacted();
 
-    $data['content'] = $this->load->view('frontsite/home/index', $data, true);
-    $this->load->view('frontsite/layout/wrapper_view', $data);
-  }
+		$data['content'] = $this->load->view('frontsite/home/index', $data, true);
+		$this->load->view('frontsite/layout/wrapper_view', $data);
+	}
 
-  public function aboutwyathservices()
-  {
-    $data['title'] = "About Wyath Services";
-    $data['about_us']             = $this->front_model->get_about_us();
-    $data['content'] = $this->load->view('frontsite/about/aboutwyathservices', $data, true);
-    $this->load->view('frontsite/layout/wrapper_view', $data);
-  }
+	public function aboutwyathservices()
+	{
+		$data['title']			= "About Wyath Services";
+		$data['about_us']		= $this->front_model->get_about_us();
+		$data['content']		= $this->load->view('frontsite/about/aboutwyathservices', $data, true);
+		$this->load->view('frontsite/layout/wrapper_view', $data);
+	}
 
-  public function boardofdirectors()
-  {
-    $data['title'] = "Board Of Directors";
-    $data['content'] = $this->load->view('frontsite/about/boardofdirectors', $data, true);
-    $this->load->view('frontsite/layout/wrapper_view', $data);
-  }
+	public function boardofdirectors()
+	{
+		$data['title'] = "Board Of Directors";
+		$data['content'] = $this->load->view('frontsite/about/boardofdirectors', $data, true);
+		$this->load->view('frontsite/layout/wrapper_view', $data);
+	}
 
-  public function boardofadvisors()
-  {
-    $data['title'] = "Board of Advisors ";
-    $data['content'] = $this->load->view('frontsite/about/boardofadvisors', $data, true);
-    $this->load->view('frontsite/layout/wrapper_view', $data);
-  }
+	public function boardofadvisors()
+	{
+		$data['title'] = "Board of Advisors ";
+		$data['content'] = $this->load->view('frontsite/about/boardofadvisors', $data, true);
+		$this->load->view('frontsite/layout/wrapper_view', $data);
+	}
 
-  public function fourpillars()
-  {
-    $data['title'] = "5 Core Pillars";
-    $data['content'] = $this->load->view('frontsite/about/fourpillars', $data, true);
-    $this->load->view('frontsite/layout/wrapper_view', $data);
-  }
+	public function fourpillars()
+	{
+		$data['title'] = "5 Core Pillars";
+		$data['content'] = $this->load->view('frontsite/about/fourpillars', $data, true);
+		$this->load->view('frontsite/layout/wrapper_view', $data);
+	}
 
-  public function keydifferentiators()
-  {
-    $data['title'] = "Key Differentiators";
-    $data['content'] = $this->load->view('frontsite/about/keydifferentiators', $data, true);
-    $this->load->view('frontsite/layout/wrapper_view', $data);
-  }
+	public function keydifferentiators()
+	{
+		$data['title'] = "Key Differentiators";
+		$data['content'] = $this->load->view('frontsite/about/keydifferentiators', $data, true);
+		$this->load->view('frontsite/layout/wrapper_view', $data);
+	}
 
-  public function ourimpact()
-  {
-    $data['title'] = "Our Impact";
-    $data['content'] = $this->load->view('frontsite/about/ourimpact', $data, true);
-    $this->load->view('frontsite/layout/wrapper_view', $data);
-  }
+	public function ourimpact()
+	{
+		$data['title'] = "Our Impact";
+		$data['content'] = $this->load->view('frontsite/about/ourimpact', $data, true);
+		$this->load->view('frontsite/layout/wrapper_view', $data);
+	}
 
-  public function chairmansmessage()
-  {
-    $data['title'] = "Chairman's Message";
-    $data['content'] = $this->load->view('frontsite/about/chairmansmessage', $data, true);
-    $this->load->view('frontsite/layout/wrapper_view', $data);
-  }
+	public function chairmansmessage()
+	{
+		$data['title'] = "Chairman's Message";
+		$data['content'] = $this->load->view('frontsite/about/chairmansmessage', $data, true);
+		$this->load->view('frontsite/layout/wrapper_view', $data);
+	}
 
-  public function partners()
-  {
-    $data['title'] = "Partners";
-    $data['content'] = $this->load->view('frontsite/about/partners', $data, true);
-    $this->load->view('frontsite/layout/wrapper_view', $data);
-  }
+	public function partners()
+	{
+		$data['title'] = "Partners";
+		$data['content'] = $this->load->view('frontsite/about/partners', $data, true);
+		$this->load->view('frontsite/layout/wrapper_view', $data);
+	}
 
-  public function areacovered()
-  {
-    $data['title'] = "Area Covered";
-    $data['content'] = $this->load->view('frontsite/about/areacovered', $data, true);
-    $this->load->view('frontsite/layout/wrapper_view', $data);
-  }
+	public function areacovered()
+	{
+		$data['title'] = "Area Covered";
+		$data['content'] = $this->load->view('frontsite/about/areacovered', $data, true);
+		$this->load->view('frontsite/layout/wrapper_view', $data);
+	}
 
-  public function news()
-  {
-    $data['title'] = "News & Notification";
-    $data['content'] = $this->load->view('frontsite/about/news', $data, true);
-    $this->load->view('frontsite/layout/wrapper_view', $data);
-  }
+	public function news()
+	{
+		$data['title'] = "News & Notification";
+		$data['content'] = $this->load->view('frontsite/about/news', $data, true);
+		$this->load->view('frontsite/layout/wrapper_view', $data);
+	}
 
-  public function initiatives()
-  {
-    $data['title'] = "Initiatives";
-    $data['initiatives']  = $this->front_model->get_initiatives();
-    $data['content'] = $this->load->view('frontsite/initiatives/initiatives', $data, true);
-    $this->load->view('frontsite/layout/wrapper_view', $data);
-  }
+	public function initiatives()
+	{
+		$data['title']				= "Initiatives";
+		$data['initiatives']  = $this->front_model->get_initiatives();
+		$data['content']			= $this->load->view('frontsite/initiatives/initiatives', $data, true);
+		$this->load->view('frontsite/layout/wrapper_view', $data);
+	}
 
-  public function services()
-  {
-    $data['title'] = "Services";
-    $data['services']  = $this->front_model->get_services();
-    $data['content'] = $this->load->view('frontsite/services/services', $data, true);
-    $this->load->view('frontsite/layout/wrapper_view', $data);
-  }
+	public function services()
+	{
+		$data['title'] = "Services";
+		$data['services']  = $this->front_model->get_services();
+		$data['content'] = $this->load->view('frontsite/services/services', $data, true);
+		$this->load->view('frontsite/layout/wrapper_view', $data);
+	}
 
-  public function contact()
-  {
-    $data['title'] = "Contact";
-    $data['content'] = $this->load->view('frontsite/contact/contact', $data, true);
-    $this->load->view('frontsite/layout/wrapper_view', $data);
-  }
+	public function contact()
+	{
+		$data['title'] = "Contact";
+		$data['content'] = $this->load->view('frontsite/contact/contact', $data, true);
+		$this->load->view('frontsite/layout/wrapper_view', $data);
+	}
 }
--- application/views/frontsite/initiatives/initiatives.php
+++ application/views/frontsite/initiatives/initiatives.php
@@ -3,7 +3,6 @@
 
   <?php $this->load->view('frontsite/about/parts/initative_nav'); ?>
 
-
   <!-- BEGIN: PAGE CONTENT -->
   <div class="c-content-box c-size-sm c-bg-white">
     <div class="container">
@@ -16,151 +15,83 @@
       </div>
     </div>
   </div>
-  <!-- BEGIN: CONTENT/FEATURES/FEATURES-4-3 -->
-  <!-- BEGIN: FEATURES 4.3 -->
-  <div id="1f" class="c-content-box c-size-md c-no-padding c-bg-white">
-    <div class="c-content-feature-4">
-      <div class="c-bg-parallax c-feature-bg c-content-right c-diagonal c-border-left-white" style="background-image: url(<?= base_url('frontsite/'); ?>assets/base/img/content/backgrounds/1.jpg)">
-      </div>
-      <div class="c-content-area c-content-left">
-      </div>
-      <div class="container">
-        <div class="c-feature-content c-left">
-          <div class="c-content-v-center">
-            <div class="c-wrapper">
-              <div class="c-body">
-                <div class="c-content-title-1">
-                  <h3 class="c-font-uppercase c-font-bold c-left"><?php echo $initiatives[0]->init_ser_title ?></h3>
-                  <div class="c-line-left">
-                  </div>
-
-                </div>
-                <div class="c-content-title-1">
-                  <p class="c-font-open c-justify">
-                    <?php echo $initiatives[0]->init_ser_desc ?>
-
-                  </p>
-                  <a href="faculty-development.html" class="btn btn-sm c-theme-btn c-btn-square c-btn-sbold">
-                    EXPLORE </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-- END: FEATURES 4.3 -->
-  <!-- END: CONTENT/FEATURES/FEATURES-4-3 -->
-  <!-- BEGIN: CONTENT/FEATURES/FEATURES-4-4 -->
-  <!-- BEGIN: FEATURES 4.4 -->
-  <div id="1s" class="c-content-box c-size-md c-no-padding c-bg-dark-2">
-    <div class="c-content-feature-4">
-      <div class="c-bg-parallax c-feature-bg c-content-left c-diagonal c-border-right-dark-2" style="background-image: url(<?= base_url('frontsite/'); ?>assets/base/img/content/backgrounds/2.jpg)">
-      </div>
-      <div class="c-content-area c-content-right">
-      </div>
-      <div class="container">
-        <div class="c-feature-content c-right">
-          <div class="c-content-v-center">
-            <div class="c-wrapper">
-              <div class="c-body">
-                <div class="c-content-title-1">
-                  <h3 class="c-font-uppercase c-font-bold c-font-white"><?php echo $initiatives[1]->init_ser_title ?></h3>
-                  <div class="c-line-right">
-                  </div>
-
-                </div>
-                <div class="c-content-title-1">
-                  <p class="c-font-open c-justify">
-                    <?php echo $initiatives[1]->init_ser_desc ?>
-                  </p>
-                  <a href="skill-development-courses.html" class="btn btn-sm c-theme-btn c-btn-square c-btn-sbold">
-                    EXPLORE </a>
-
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-- END: FEATURES 4.4 -->
-  <!-- END: CONTENT/FEATURES/FEATURES-4-4 -->
-  <!-- BEGIN: CONTENT/FEATURES/FEATURES-4-3 -->
-  <!-- BEGIN: FEATURES 4.3 -->
-  <div id="1e" class="c-content-box c-size-md c-no-padding c-bg-white">
-    <div class="c-content-feature-4">
-      <div class="c-bg-parallax c-feature-bg c-content-right c-diagonal c-border-left-white" style="background-image: url(<?= base_url('frontsite/'); ?>assets/base/img/content/backgrounds/3.jpg)">
-      </div>
-      <div class="c-content-area c-content-left">
-      </div>
-      <div class="container">
-        <div class="c-feature-content c-left">
-          <div class="c-content-v-center">
-            <div class="c-wrapper">
-              <div class="c-body">
-                <div class="c-content-title-1">
-                  <h3 class="c-font-uppercase c-font-bold c-left"><?php echo $initiatives[2]->init_ser_title ?></h3>
-                  <div class="c-line-left">
-                  </div>
-
-                </div>
-                <div class="c-content-title-1">
-                  <p class="c-font-open c-justify">
-                    <?php echo $initiatives[2]->init_ser_desc ?>
-                  </p>
-                  <a href="Entrepreneurship-development.html" class="btn btn-sm c-theme-btn c-btn-square c-btn-sbold">
-                    EXPLORE </a>
-
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-- END: FEATURES 4.3 -->
-  <!-- END: CONTENT/FEATURES/FEATURES-4-3 -->
-  <!-- BEGIN: CONTENT/FEATURES/FEATURES-4-4 -->
-  <!-- BEGIN: FEATURES 4.4 -->
-  <div id="1y" class="c-content-box c-size-md c-no-padding c-bg-dark-2">
-    <div class="c-content-feature-4">
-      <div class="c-bg-parallax c-feature-bg c-content-left c-diagonal c-border-right-dark-2" style="background-image: url(<?= base_url('frontsite/'); ?>assets/base/img/content/backgrounds/4.jpg)">
-      </div>
-      <div class="c-content-area c-content-right">
-      </div>
-      <div class="container">
-        <div class="c-feature-content c-right">
-          <div class="c-content-v-center">
-            <div class="c-wrapper">
-              <div class="c-body">
-                <div class="c-content-title-1">
-                  <h3 class="c-font-uppercase c-font-bold c-font-white"><?php echo $initiatives[3]->init_ser_title ?></h3>
-                  <div class="c-line-right">
-                  </div>
-
-                </div>
-                <div class="c-content-title-1">
-                  <p class="c-font-open c-justify">
-                    <?php echo $initiatives[3]->init_ser_desc ?>
-                  </p>
-                  <a href="Youth-Empowerment-2.html" class="btn btn-sm c-theme-btn c-btn-square c-btn-sbold">
-                    EXPLORE </a>
-
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <!-- END: FEATURES 4.3 -->
-  <!-- END: CONTENT/FEATURES/FEATURES-4-3 -->
   <!-- END: PAGE CONTENT -->
+
+  <?php if (valArr($initiatives)) : ?>
+    <?php foreach ($initiatives as $key => $initiative) : ?>
+      <?php if (($key + 2) % 2 == 0) : ?>
+        <!-- BEGIN: FEATURES 1 -->
+        <div id="1f" class="c-content-box c-size-md c-no-padding c-bg-white">
+          <div class="c-content-feature-4">
+            <div class="c-bg-parallax c-feature-bg c-content-right c-diagonal c-border-left-white" style="background-image: url(<?= base_url('frontsite/'); ?>assets/base/img/content/backgrounds/1.jpg)">
+            </div>
+            <div class="c-content-area c-content-left">
+            </div>
+            <div class="container">
+              <div class="c-feature-content c-left">
+                <div class="c-content-v-center">
+                  <div class="c-wrapper">
+                    <div class="c-body">
+                      <div class="c-content-title-1">
+                        <h3 class="c-font-uppercase c-font-bold c-left"><?php echo $initiative->init_ser_title ?></h3>
+                        <div class="c-line-left">
+                        </div>
+
+                      </div>
+                      <div class="c-content-title-1">
+                        <p class="c-font-open c-justify">
+                          <?php echo $initiative->init_ser_desc ?>
+
+                        </p>
+                        <a href="faculty-development.html" class="btn btn-sm c-theme-btn c-btn-square c-btn-sbold">
+                          EXPLORE </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- END: FEATURES 1 -->
+      <?php else : ?>
+        <!-- BEGIN: FEATURES 2 -->
+        <div id="1s" class="c-content-box c-size-md c-no-padding c-bg-dark-2">
+          <div class="c-content-feature-4">
+            <div class="c-bg-parallax c-feature-bg c-content-left c-diagonal c-border-right-dark-2" style="background-image: url(<?= base_url('frontsite/'); ?>assets/base/img/content/backgrounds/2.jpg)">
+            </div>
+            <div class="c-content-area c-content-right">
+            </div>
+            <div class="container">
+              <div class="c-feature-content c-right">
+                <div class="c-content-v-center">
+                  <div class="c-wrapper">
+                    <div class="c-body">
+                      <div class="c-content-title-1">
+                        <h3 class="c-font-uppercase c-font-bold c-font-white"><?php echo $initiative->init_ser_title ?></h3>
+                        <div class="c-line-right">
+                        </div>
+
+                      </div>
+                      <div class="c-content-title-1">
+                        <p class="c-font-open c-justify">
+                          <?php echo $initiative->init_ser_desc ?>
+                        </p>
+                        <a href="skill-development-courses.html" class="btn btn-sm c-theme-btn c-btn-square c-btn-sbold">
+                          EXPLORE </a>
+
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- END: FEATURES 2 -->
+      <?php endif; ?>
+    <?php endforeach; ?>
+  <?php endif; ?>
+
 </div>
 <!-- END: PAGE CONTAINER -->
