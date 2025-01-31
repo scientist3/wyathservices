134a02f8164992bbda063985a70f9a20d60e60b8
Aamir Bashir <ksaak33@gmail.com>
2025-02-01 02:07:13 +0530

    Enhance dashboard with candidate statistics and improve layout

--- application/controllers/admin/Dashboard.php
+++ application/controllers/admin/Dashboard.php
@@ -1,22 +1,54 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
-
+/**
+ * @property CI_Session $session
+ * @property CI_Input $input
+ * @property CI_Form_validation $form_validation
+ * @property CI_DB_query_builder $db
+ * @property CommonModel $CommonModel
+ * @property BatchModel $BatchModel
+ * @property CourseModel $CourseModel
+ * @property TrainingCenterModel $TrainingCenterModel
+ * @property BatchModel $BatchModel
+ * @property BatchMappingModel $BatchMappingModel
+ * @property CandidateModel $CandidateModel
+ */
 class Dashboard extends CI_Controller
 {
-  public function __construct()
-  {
-    parent::__construct();
-    if ($this->session->userdata('isLogIn') == false || $this->session->userdata('user_role') != 1)
-      redirect('login/logout');
-    $this->user_id = $this->session->userdata('user_id');
-  }
-  public function index()
-  {
-    $this->data['title']    = ('Dashboard');
-    // $this->data['subtitle']	= ('Add New Candidate');
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model(array(
+			'admin/candidate/CommonModel',
+			'admin/candidate/BatchModel',
+			'admin/candidate/CourseModel',
+			'admin/candidate/TrainingCenterModel',
+			'admin/candidate/BatchMappingModel',
+			'admin/candidate/CandidateModel'
+		));
+		if ($this->session->userdata('isLogIn') == false || $this->session->userdata('user_role') != 1)
+			redirect('login/logout');
+		$this->user_id = $this->session->userdata('user_id');
+	}
+	public function index()
+	{
+		$this->data['title']    = ('Dashboard');
+		// $this->data['subtitle']	= ('Add New Candidate');
+		$candidateStats = [
+			'total_students'            => $this->CandidateModel->getTotalStudents(),
+			'total_enrolled_students'    => $this->CandidateModel->getTotalEnrolledStudents(),
+			'total_male_students'        => $this->CandidateModel->getTotalMaleStudents(),
+			'total_female_students'      => $this->CandidateModel->getTotalFemaleStudents(),
+			'total_trans_students'      => $this->CandidateModel->getTotalTransagenderStudents(),
+			'completed_training'        => $this->CandidateModel->getCompletedTrainingCount(),
+			'completed_assessment'      => $this->CandidateModel->getCompletedAssessmentCount(),
+			'completed_certified'        => $this->CandidateModel->getCompletedCertifiedCount(),
+			'placement_completed'        => $this->CandidateModel->getPlacementCompletedCount(),
+			'tracking_completed'        => $this->CandidateModel->getPlacementTrackingCompletedCount(),
+		];
+		$this->data['candidate_stats'] = $candidateStats;
+		$this->data['content'] = $this->load->view('admin/dashboard_view', $this->data, true);
 
-    $this->data['content'] = $this->load->view('admin/dashboard_view', $this->data, true);
-
-    $this->load->view('admin/layout/wrapper', $this->data);
-  }
+		$this->load->view('admin/layout/wrapper', $this->data);
+	}
 }
--- application/views/admin/dashboard_view.php
+++ application/views/admin/dashboard_view.php
@@ -1,9 +1,111 @@
 <section class="content">
-  <div class="card card-dark">
-    <div class="card-header">
-      <h3 class="card-title">Dashboard</h3>
-    </div>
-    <div class="card-body"></div>
-    <div class="card-footer"></div>
-  </div>
+	<div class="row">
+		<div class="col-md-3 col-sm-6 col-12">
+			<div class="info-box">
+				<span class="info-box-icon bg-info"><i class="fas fa-users"></i></span>
+				<div class="info-box-content">
+					<span class="info-box-text">Total Students</span>
+					<span class="info-box-number"><?php echo number($candidate_stats['total_students'] ?? 0); ?></span>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-md-3 col-sm-6 col-12">
+			<div class="info-box">
+				<span class="info-box-icon bg-success"><i class="fas fa-graduation-cap"></i></span>
+				<div class="info-box-content">
+					<span class="info-box-text">Total Entolled</span>
+					<span class="info-box-number"><?php echo number($candidate_stats['total_enrolled_students'] ?? 0); ?></span>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-md-3 col-sm-6 col-12">
+			<div class="info-box">
+				<span class="info-box-icon bg-success"><i class="fas fa-graduation-cap"></i></span>
+				<div class="info-box-content">
+					<span class="info-box-text">Training Completed</span>
+					<span class="info-box-number"><?php echo number($candidate_stats['completed_training'] ?? 0); ?></span>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-md-3 col-sm-6 col-12">
+			<div class="info-box">
+				<span class="info-box-icon bg-dark"><i class="fas fa-clipboard-check"></i></span>
+				<div class="info-box-content">
+					<span class="info-box-text">Assessment Completed</span>
+					<span class="info-box-number"><?php echo number($candidate_stats['completed_assessment'] ?? 0); ?></span>
+				</div>
+
+			</div>
+
+		</div>
+
+		<div class="col-md-3 col-sm-6 col-12">
+			<div class="info-box">
+				<span class="info-box-icon bg-olive"><i class="fas fa-certificate"></i></span>
+				<div class="info-box-content">
+					<span class="info-box-text">Certificate Completed</span>
+					<span class="info-box-number"><?php echo number($candidate_stats['completed_certified'] ?? 0); ?></span>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-md-3 col-sm-6 col-12">
+			<div class="info-box">
+				<span class="info-box-icon bg-teal"><i class="fas fa-briefcase"></i></span>
+				<div class="info-box-content">
+					<span class="info-box-text">Placement Completed</span>
+					<span class="info-box-number"><?php echo number($candidate_stats['placement_completed'] ?? 0); ?></span>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-3 col-sm-6 col-12">
+			<div class="info-box">
+				<span class="info-box-icon bg-navy"><i class="fas fa-map-marker-alt"></i></span>
+				<div class="info-box-content">
+					<span class="info-box-text">Tracking Completed</span>
+					<span class="info-box-number"><?php echo number($candidate_stats['tracking_completed'] ?? 0); ?></span>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-3 col-sm-6 col-12">
+			<div class="info-box">
+				<span class="info-box-icon bg-teal"><i class="fas fa-child"></i></span>
+				<div class="info-box-content">
+					<span class="info-box-text">Total Males</span>
+					<span class="info-box-number"><?php echo number($candidate_stats['total_male_students'] ?? 0); ?></span>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-md-3 col-sm-6 col-12">
+			<div class="info-box">
+				<span class="info-box-icon bg-purple"><i class="fas fa-female"></i></span>
+				<div class="info-box-content">
+					<span class="info-box-text">Total Females</span>
+					<span class="info-box-number"><?php echo number($candidate_stats['total_female_students'] ?? 0); ?></span>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-md-3 col-sm-6 col-12">
+			<div class="info-box">
+				<span class="info-box-icon bg-blue"><i class="fas fa-transgender"></i></span>
+				<div class="info-box-content">
+					<span class="info-box-text">Total Transgenders</span>
+					<span class="info-box-number"><?php echo number($candidate_stats['total_trans_students'] ?? 0); ?></span>
+				</div>
+			</div>
+		</div>
+
+	</div>
+	<div class="card card-dark d-none">
+		<div class="card-header">
+			<h3 class="card-title">Dashboard</h3>
+		</div>
+		<div class="card-body"></div>
+		<div class="card-footer"></div>
+	</div>
 </section>
